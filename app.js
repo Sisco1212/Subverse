7657ad7bcccae408d47b98123dfc1e8adee0bf58
Sisco1212 <sitopatrick2004@gmail.com>
2026-09-14 16:27:28 +0100

    cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
 import arcjetMiddleware from './middlewares/arcject.middleware.js';
 import workflowRouter from './routes/workflow.routes.js';
+import cors from "cors";
 
 
 const app = express();
@@ -17,6 +18,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(arcjetMiddleware);
 
+
+app.use(cors({
+  origin: "http://localhost:3000",
+}));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
